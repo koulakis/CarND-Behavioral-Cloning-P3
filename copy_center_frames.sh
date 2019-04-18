954b9ca05d46906adec081e94fc191d90cb26251
mariosk <marios.koulakis@gmail.com>
2019-04-18 02:21:01 +0200

    Implements custom generator with currently used transforms.

--- copy_center_frames.sh
+++ copy_center_frames.sh
@@ -1,5 +1,5 @@
-ls my-videos | xargs -I {} mkdir -p my-videos-small/{}
-ls my-videos | xargs -I {} mkdir -p my-videos-small/{}/IMG
+ls my-videos | xargs -I {} mkdir -p my-videos-center/{}
+ls my-videos | xargs -I {} mkdir -p my-videos-center/{}/IMG
 
-ls my-videos | xargs -I{} sh -c "cp my-videos/{}/IMG/center*.jpg my-videos-small/{}/IMG/"
-ls my-videos | xargs -I{} sh -c "cp my-videos/{}/driving_log.csv my-videos-small/{}/"
+ls my-videos | xargs -I{} sh -c "cp my-videos/{}/IMG/center*.jpg my-videos-center/{}/IMG/"
+ls my-videos | xargs -I{} sh -c "cp my-videos/{}/driving_log.csv my-videos-center/{}/"
